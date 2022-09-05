d196a1ede31d4e51c7adae10fa2bbc88b2cf8c5b
Daniela Lopez <dlope0831@gmail.com>
2022-09-05 16:31:26 -0500

    adding template

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+// const mockData = {
+//   name: 'Lernantino',
+//   github: 'lernantino',
+//   projects: []
+// }
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -126,14 +131,15 @@ Add a New Project
     });
 };
 
+// const pageHTML = generatePage(mockData);
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
