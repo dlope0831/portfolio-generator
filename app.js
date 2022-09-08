094d8a3b36b0053b419d007a70ce83352e3b3bd3
Daniela Lopez <dlope0831@gmail.com>
2022-09-08 17:04:49 -0500

    finishing site

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
-// const mockData = {
-//   name: 'Lernantino',
-//   github: 'lernantino',
-//   projects: []
-// }
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -131,15 +126,21 @@ Add a New Project
     });
 };
 
-// const pageHTML = generatePage(mockData);
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
